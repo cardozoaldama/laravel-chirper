389666eed7a46f09631ec7a8d505a0d5a59a9d38
Fernando Cardozo <nuevo_10000@outlook.com>
2026-04-14 12:07:48 -0300

    feat(controller): add chirp and return simple view
    Also, updated routes so the new chirp controller could return responses.
    Handling the data as its responsability.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn() => view('welcome'));
+Route::get('/', fn () => view('welcome'));
 
-Route::get('home', fn() => view('home'));
+Route::get('home', [ChirpController::class, 'index']);
