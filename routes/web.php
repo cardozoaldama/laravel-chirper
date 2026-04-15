ad447c470f0415c4646d9687b51e531838a08c25
Fernando Cardozo <nuevo_10000@outlook.com>
2026-04-15 11:06:29 -0300

    refactor(route): change the root page with chirp.index

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,5 @@
 use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => view('welcome'));
-
-Route::get('home', [ChirpController::class, 'index']);
+Route::get('/', [ChirpController::class, 'index']);
 Route::post('/chirps', [ChirpController::class, 'store']);
