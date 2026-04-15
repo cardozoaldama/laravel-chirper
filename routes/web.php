219579baa4a144955113adf34dbbe03f9f0e35ee
Fernando Cardozo <nuevo_10000@outlook.com>
2026-04-15 14:10:52 -0300

    feat(route): add register auth with middleware guest

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,17 @@
 
 use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\Register;
 
 Route::get('/', [ChirpController::class, 'index']);
 
 Route::resource('chirps', ChirpController::class)
     ->only(['store', 'edit', 'update', 'destroy']);
+
+// Registration routes
+Route::view('/register', 'auth.register')
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/register', Register::class)
+    ->middleware('guest');
